c8c3c72e5c909a48b1c819ff4e1f373fe9baa94c
Lennart Jakobsson <len.nart@hotmail.com>
2015-10-09 10:53:33 +0200

    upt

---
+++ OOMULab2/src/grupp4/othello/Othello.java
@@ -0,0 +1,24 @@
+package grupp4.othello;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Lennart
+ */
+public class Othello extends Application{
+    
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+    
+}
