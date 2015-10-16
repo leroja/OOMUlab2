80386651a93182990d4124164ef82a03895a1ce9
alexander <alexander@10.10.202.250>
2015-10-16 09:46:07 +0200

    Merge origin/master
    Conflicts:
    	OOMULab2/src/grupp4/othello/Othello.java

--- OOMULab2/src/grupp4/othello/controller/GameManager.java
+++ OOMULab2/src/grupp4/othello/controller/GameManager.java
@@ -5,24 +5,44 @@
  */
 package grupp4.othello.controller;
 
+import grupp4.othello.model.GameGrid;
+import grupp4.othello.model.Player;
 import grupp4.othello.view.*;
 import javafx.util.Pair;
 
 /**
- *
+ *  
  * @author Lennart
  */
 
 public class GameManager {
+    
+    public void othelloManager(Player p1, Player p2){
+        
+    }
 
     public void manage(){
 
         
     }
     
-    
-    public void notifyGameBoard(char[][] grid){
-        GameBoard board = new GameBoard();
-        board.onUppdateInGameGrid(grid);
+    private void gameOver(GameGrid grid,Player p1, Player p2){
+        if(!(grid.moreAvailableMoves(p1.getMarkörID()))&&!(grid.moreAvailableMoves(p2.getMarkörID()))){
+                    WinnerDialog winner = new WinnerDialog();
+                    if (grid.winner() == 'B'){
+                        winner.displayWinnerDialog(p1.getName());
+                        System.exit(0);
+                }
+                    else if(grid.winner() == 'W'){
+                         winner.displayWinnerDialog(p2.getName());
+                         System.exit(0);
+                    }
+                    else{
+                        DrawnDialog draw = new DrawnDialog();
+                        draw.DisplayDrawDialog();
+                        System.exit(0);
+                    }
+                   
+                }
     }
 }
